d4a6dbd4795869b4482a4dc96d27c9891117d841
AhAh10 <ahmadi2001@yahoo.com>
2017-07-25 12:05:27 +0200

    Conflicts

---
+++ LMS-Entities/Classroom.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS_Entities
+{
+    public class Classroom
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required, MaxLength(255)]
+        public string Name { get; set; }
+        [Required, MaxLength(255)]
+        public string Location { get; set; }
+    }
+}
