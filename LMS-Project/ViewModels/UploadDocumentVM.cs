54083755b2162678f1f90058429451af91f5f488
AhAh10 <ahmadi2001@yahoo.com>
2017-07-31 10:02:25 +0200

    Missing files

---
+++ LMS-Project/ViewModels/UploadDocumentVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LMS_Project.ViewModels
+{
+    public class UploadDocumentVM
+    {
+        public HttpPostedFileBase File { get; set; }
+        public int CourseID { get; set; }
+
+        public string DocumentName
+        {
+            get
+            {
+                if (File != null)
+                    return File.FileName;
+                else
+                    return String.Empty;
+            }
+            private set { }
+        }
+
+    }
+}
