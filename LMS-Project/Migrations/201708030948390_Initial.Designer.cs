7e68c475adfa0018d7ef10249939897d08aaa671
Tarek Bouzo <tarekbozo@gmail.com>
2017-08-07 13:46:05 +0200

    add aninymous Publisher

--- LMS-Project/Migrations/201708030948390_Initial.Designer.cs
+++ LMS-Project/Migrations/201708030948390_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace LMS_Project.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Initialforce : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initialforce));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201708021206110_Initial-force"; }
+            get { return "201708030948390_Initial"; }
         }
         
         string IMigrationMetadata.Source
