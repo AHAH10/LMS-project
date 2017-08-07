543a87f95ff7b71956ab0b3026ef3a4a0bcd5847
AhAh10 <ahmadi2001@yahoo.com>
2017-08-07 10:54:18 +0200

    Created Notification table, Repo and Controller

---
+++ LMS-Project/Repositories/NotificationRepository.cs
@@ -0,0 +1,37 @@
+﻿using LMS_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_Project.Repositories
+{
+    public class NotificationRepository : IDisposable
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
+        private void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                db.Dispose();
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
