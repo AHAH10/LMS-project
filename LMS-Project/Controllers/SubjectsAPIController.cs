ace768777b55c321ae61a6926e60808ee085031b
Gommash <a.lekedal@gmail.com>
2017-08-09 12:03:42 +0200

    UsersAPI Have a new method - GetTeachers(string subject) - courses.js is rewritten

---
+++ LMS-Project/Controllers/SubjectsAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using LMS_Project.Models.LMS;
+using LMS_Project.Repositories;
+
+namespace LMS_Project.Controllers
+{
+    [System.Web.Mvc.ValidateAntiForgeryToken]
+    [Authorize(Roles="Admin")] //Only an Admin can access the api
+    public class SubjectsAPIController : ApiController
+    {
+        [HttpGet]
+        public List<Subject> GetAllSubjects() //VS 17 returns access violation if a User is returned through api, Courses contains a teacher...
+        {
+            List<Subject> _subjects = new List<Subject>();
+            foreach(Subject s in new SubjectsRepository().Subjects())
+            {
+                Subject tempS = new Subject();
+                tempS.ID = s.ID;
+                tempS.Name = s.Name;
+                _subjects.Add(tempS);
+                tempS = null;
+            }
+            return _subjects;
+        }
+    }
+}
--- LMS-Project/Scripts/Angular/Courses.js
+++ LMS-Project/Scripts/Angular/Courses.js
@@ -9,137 +9,79 @@
             $scope.reverse = ($scope.myOrderBy === type) ? !$scope.reverse : false;
         };
         $scope.getData = function () {
-            $http.get('/api/C_And_S_API/GetAllCourses')
+            $http.get('/api/CoursesAPI/GetAllCourses')
                 .then(function (response) {
                     $scope.courses = JSON.parse(JSON.stringify(response.data));
                 })
         };
     }]);
+    //Edit
+    app.controller("Course_Edit_Ctrl", ["$scope", "$http", function ($scope, $http) {
+        $scope.reverse = true;
+
+        $scope.orderByMe = function (type) {
+            alert(type);
+            $scope.myOrderBy = type;
+            $scope.reverse = ($scope.myOrderBy === type) ? !$scope.reverse : false;
+        };
+
+        $scope.Start = function (sID) {
+            $scope.selectedSubject = sID;
+            $http.get('/api/UsersAPI/GetAvailableTeachers?subjectID=' + $scope.selectedSubject)
+                .then(function (response) {
+                    $scope.teachers = JSON.parse(JSON.stringify(response.data));
+                });            
+        }
+
+    }]);
     //Create
     app.controller("Course_Create_Ctrl", ["$scope", "$http", function ($scope, $http) {
+        $scope.Treverse = true;
+        $scope.Sreverse = true;
 
-        $scope.selectedSubject = document.getElementById("subjects").value;
-        var start = true;
-        var sortSAsc = true;
-        var sortTAsc = true;
+        $scope.orderBySubject = function (type) {
+            orderBy(type,"Subject");
+        };
+        $scope.orderByTeacher = function (type) {
+            orderBy(type, "Teacher");
+        };
+        function orderBy(t,o) {
+            if (o == "Teacher") {
+                $scope.TheOrderByTeacher = t;
+                $scope.Treverse = ($scope.TheOrderByTeacher === t) ? !$scope.Treverse : false;
+            }
+            else{
+                $scope.TheOrderBySubject = t;
+                $scope.Sreverse = ($scope.TheOrderBySubject === t) ? !$scope.Sreverse : false;
+            }
+        }
 
         $scope.Start = function () {
-            sortTheList("subjects");
-            update();
-        }
-
-        function getAvaibleTeachers() {
-            $http.get('/api/C_And_S_API/GetAvailableTeachersWithLessInfo?subjectID=' + $scope.selectedSubject)
+            $http.get('/api/SubjectsAPI/GetAllSubjects')
                 .then(function (response) {
-                    $scope.teachers = JSON.parse(JSON.stringify(response.data)); //Any references will be converted into objects
-
-                    var op = [];
-
-                    for (var i = 0; i < $scope.teachers.length; i++) {
-                        var _option = { value: "", text: "" };
-                        _option.value = $scope.teachers[i].Id;
-                        _option.text = $scope.teachers[i].FirstName + " " + $scope.teachers[i].LastName;
-                        op.push(_option);
-                    }
-                    updateDropDownList("teachers", op);
-                    if (start) {
-                        sortTheList("teachers");
-                        start = false;
-                    }
+                    $scope.subjects = JSON.parse(JSON.stringify(response.data));
+                    $scope.subject = $scope.subjects[0].ID;
+                    $http.get('/api/UsersAPI/GetAvailableTeachers?subjectID=' + $scope.subject)
+                        .then(function (response) {
+                            $scope.teachers = JSON.parse(JSON.stringify(response.data));
+                            orderBy("Name", "Subject");
+                            orderBy("LastName", "Teacher");
+                        });
                 });
         }
-        function sort(options, desc) {
-            if (desc == true) {
-                options.sort(function (a, b) {
-                    var textA = a.text.toLowerCase(), textB = b.text.toLowerCase()
-                    if (textA < textB)
-                        return -1
-                    if (textA > textB)
-                        return 1
-                    return 0
+
+        function uData() {
+            $http.get('/api/UsersAPI/GetAvailableTeachers?subjectID=' + $scope.subject)
+                .then(function (response) {
+                    $scope.teachers = JSON.parse(JSON.stringify(response.data));
+
                 });
-            }
-            else {
-                options.sort(function (a, b) {
-                    var textA = a.text.toLowerCase(), textB = b.text.toLowerCase()
-                    if (textA > textB)
-                        return -1
-                    if (textA < textB)
-                        return 1
-                    return 0
-                });
-            }
-            return options;
         }
 
-        function update() {
-            $scope.selectedSubject = document.getElementById("subjects").value;
-            getAvaibleTeachers();
+        $scope.update = function () {
+            uData();
         }
-        $scope.Update = function () {
-            update();
-        };
-        //an ng-repeat select list would had been easier, but if javascript is disabled on the client, the dropdown list should still work
-        function sortTheList(sID) {
-            if (sID == "teachers") {
-                var op = [];
 
-                for (var i = 0; i < document.getElementById("teachers").getElementsByTagName("option").length; i++) {
-                    var _option = { value: "", text: "" };
-                    _option.value = document.getElementById("teachers").getElementsByTagName("option")[i].value;
-                    _option.text = document.getElementById("teachers").getElementsByTagName("option")[i].text;
-                    op.push(_option);
-                }
-                if (sortTAsc == true) {
-                    op = sort(op, true);
-                    sortTAsc = false;
-                }
-                else {
-                    op = sort(op, false);
-                    sortTAsc = true;
-                }
-                updateDropDownList("teachers", op);
-            }
-            else {
-                var opS = [];
-
-                for (var i = 0; i < document.getElementById("subjects").getElementsByTagName("option").length; i++) {
-                    var _option = { value: "", text: "" };
-                    _option.value = document.getElementById("subjects").getElementsByTagName("option")[i].value;
-                    _option.text = document.getElementById("subjects").getElementsByTagName("option")[i].text;
-                    opS.push(_option);
-                }
-                if (sortSAsc == true) {
-                    opS = sort(opS, true);
-                    sortSAsc = false;
-                }
-                else {
-                    opS = sort(opS, false);
-                    sortSAsc = true;
-                }
-                updateDropDownList("subjects", opS);
-            }
-        }
-        $scope.SortList = function (sID) {
-            sortTheList(sID);
-        };
-
-        function updateDropDownList(select_id, optionsList) {
-            var list = document.getElementById(select_id);
-            //Remove all options from the dropdownlist
-            if (list.getElementsByTagName("option") != null) {
-                while (list.firstChild) {
-                    list.removeChild(list.firstChild);
-                }
-            }
-            //create and append new options
-            for (var i = 0; i < optionsList.length; i++) {
-                var element = document.createElement("option");
-                element.textContent = optionsList[i].text;
-                element.value = optionsList[i].value;
-                list.appendChild(element);
-            }
-        }
     }]);
 
 }());
